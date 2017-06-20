98f4d705ca6b7472df656d35e20e46c05df0b3c8
Giuseppe Grilli <giugrilli@gmail.com>
2017-06-19 20:08:38 -0400

    added json and jsonb

--- lib/activeadmin/resource_dsl.rb
+++ lib/activeadmin/resource_dsl.rb
@@ -7,7 +7,7 @@ module ActiveAdmin
       before_save do |object,args|
         request_namespace = object.class.name.underscore.gsub('/', '_')
         if params.key? request_namespace
-          object.class.columns_hash.select {|key,attr| attr.type == :hstore}.keys.each do |key|
+          object.class.columns_hash.select {|key,attr| [:hstore, :json, :jsonb].include? attr.type }.keys.each do |key|
             if params[request_namespace].key? key
               json_data = params[request_namespace][key]
               data = if json_data == 'null' or json_data.blank?
